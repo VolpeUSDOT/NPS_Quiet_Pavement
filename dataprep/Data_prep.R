d02e50bb25897fadac668e0f95eb4e51407774ec
dflynn-volpe <daniel.flynn@dot.gov>
2019-06-13 11:01:40 -0400

    Initial commit

---
+++ dataprep/Data_prep.R
@@ -0,0 +1,5 @@
+# Initial setup
+
+dataloc = 'C:/Quielt_Pavement_Local' # Change this to shared network drive when access is granted. Will have 'Data Summary QP Re-Analysis.xlsx' in it
+codeloc = '~/git/Quiet_Pavement' 
+
